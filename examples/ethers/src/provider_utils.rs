aa8968906fb09d6a9cf8bfcd0c884c37ed3de078
sergey-astapov <serg.astapov@gmail.com>
2023-06-02 21:54:40 +0800

    - changes

--- examples/ethers/src/provider_utils.rs
+++ examples/ethers/src/provider_utils.rs
@@ -7,11 +7,14 @@ use eyre::Result;
 use log::*;
 use std::{convert::TryFrom, sync::Arc};
 
-const RPC_URL: &str = "http://127.0.0.1:8545/";
+const RPC_URL: &str =
+    //"https://eth-mainnet.g.alchemy.com/v2/TtK-PVc3lbV2nb7V_qUwTUALYEEBAySG";
+    "http://127.0.0.1:8545/";
 const WETH_ADDR: &str = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 const USDC_ADDR: &str = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 const MY_ADDR: &str = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
 const PRIVATE_KEY: &str = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+const USDC_WHALE: &str = "0xcffad3200574698b78f32232aa9d63eabd290703";
 
 pub async fn provider_calls() -> Result<()> {
     let provider = Provider::<Http>::try_from(RPC_URL)?;
@@ -47,8 +50,7 @@ pub async fn provider_calls() -> Result<()> {
     Ok(())
 }
 
-pub async fn contract_load() -> Result<()> {
-    abigen!(
+abigen!(
         IERC20,
         r#"[
             function totalSupply() external view returns (uint256)
@@ -62,15 +64,25 @@ pub async fn contract_load() -> Result<()> {
         ]"#,
     );
 
+pub async fn contract_load() -> Result<()> {
     let provider = Provider::<Http>::try_from(RPC_URL)?;
     let client = Arc::new(provider);
-    let address: Address = WETH_ADDR.parse()?;
-    let contract = IERC20::new(address, client);
-    let total_supply = contract.total_supply().call().await?;
+    let weth_address: Address = WETH_ADDR.parse()?;
+    let weth_contract = IERC20::new(weth_address, Arc::clone(&client));
+    let total_supply = weth_contract.total_supply().call().await?;
     info!("total_supply={} eth", utils::format_units(total_supply, "ether").unwrap());
 
-    let balance = contract.balance_of(MY_ADDR.parse()?).call().await?;
+    let balance = weth_contract.balance_of(MY_ADDR.parse()?).call().await?;
     info!("balance={} eth", utils::format_units(balance, "ether").unwrap());
 
+    let usdc_address: Address = USDC_ADDR.parse()?;
+    let usdc_contract = IERC20::new(usdc_address, Arc::clone(&client));
+    let whale_balance = usdc_contract.balance_of(USDC_WHALE.parse()?).call().await?;
+    info!("whale_balance={}", whale_balance);
+
+    Ok(())
+}
+
+pub async fn fund_erc20() -> Result<()> {
     Ok(())
 }
