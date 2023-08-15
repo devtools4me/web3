12fe7e5677562b4fac61b1bcb8191ca389f46032
sergey-astapov <serg.astapov@gmail.com>
2023-08-15 15:14:10 +0800

    - changes

--- dydx/dydx-frontend/src/data_source.rs
+++ dydx/dydx-frontend/src/data_source.rs
@@ -5,10 +5,24 @@ use crate::gloonet::GlooNetDydxApi;
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-static CLIENT: MockDydxApi = MockDydxApi {};
-//static CLIENT: GlooNetDydxApi = GlooNetDydxApi {};
+#[derive(Clone, Copy)]
+enum Env {
+    Mock,
+    Dev,
+    Prod
+}
+
+static ENV: Env = Env::Mock;
+//static ENV: Env = Env::Dev;
 
 pub fn get_data() -> Result<Vec<Ohlc>> {
-    let response = CLIENT.get_ohlc_data();
+    let response = dydx_api_client(ENV).get_ohlc_data();
     Ok(response)
+}
+
+fn dydx_api_client(env: Env) -> Box<dyn DydxApi + Sync> {
+    match env {
+        Env::Mock => Box::new(MockDydxApi {}),
+        _ => Box::new(GlooNetDydxApi {})
+    }
 }
