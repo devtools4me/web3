e23ecd8b1f909b46bf682276495334830ff7891a
sergey-astapov <serg.astapov@gmail.com>
2023-08-20 21:35:27 +0800

    - changes

--- dydx/dydx-frontend/src/main.rs
+++ dydx/dydx-frontend/src/main.rs
@@ -1,6 +1,7 @@
 use log::error;
 use yew::prelude::*;
 
+use view::ohlc::OhlcView;
 use dydx_api::types::Ohlc;
 use utils::api_utils::fetch_single_api_response;
 
@@ -8,86 +9,9 @@ mod data_source;
 pub mod mock;
 mod client;
 mod utils;
-
-#[derive(Properties, PartialEq)]
-struct OhlcListProps {
-    ohlc_data: Vec<Ohlc>,
-}
-
-#[function_component(OhlcList)]
-fn ohlc_list(OhlcListProps { ohlc_data }: &OhlcListProps) -> Html {
-    let ohlc_data_html = ohlc_data.iter().map(|x| {
-        html! {
-            <tr>
-                <td>{x.open.clone()}</td>
-                <td>{x.high.clone()}</td>
-                <td>{x.low.clone()}</td>
-                <td>{x.close.clone()}</td>
-                <td>{x.timestamp.clone()}</td>
-            </tr>
-        }
-    });
-    html! {
-        <div>
-            <table class="table is-hoverable is-striped">
-                <thead>
-                    <tr>
-                        <th>{"Open"}</th>
-                        <th>{"High"}</th>
-                        <th>{"Low"}</th>
-                        <th>{"Close"}</th>
-                        <th>{"Timestamp"}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {for ohlc_data_html}
-                </tbody>
-            </table>
-        </div>
-    }
-}
-
-#[function_component(App)]
-fn app() -> Html {
-    let market = "BTC-USD";
-    let resolution = "1MIN";
-    let ohlc_data = use_state(|| vec![]);
-    {
-        let ohlc_data = ohlc_data.clone();
-        use_effect_with_deps(
-            move |_| {
-                let ohlc_data = ohlc_data.clone();
-                wasm_bindgen_futures::spawn_local(async move {
-                    match fetch_single_api_response::<Vec<Ohlc>>(
-                        format!("/candles/{}/{}", market, resolution)
-                            .as_str(),
-                    )
-                        .await
-                    {
-                        Ok(fetched_data) => {
-                            ohlc_data.set(fetched_data);
-                        }
-                        Err(e) => {
-                            error!("{e}")
-                        }
-                    }
-                });
-            },
-            (),
-        );
-    }
-
-    html! {
-        <div class="section">
-            <div class="container">
-                <h1 class="title">{"Main page"}</h1>
-                <OhlcList ohlc_data={(*ohlc_data).clone()} />
-            </div>
-        </div>
-    }
-}
+mod view;
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
-    yew::Renderer::<App>::new().render();
+    yew::Renderer::<OhlcView>::new().render();
 }
---
+++ dydx/dydx-frontend/src/view/ohlc.rs
@@ -0,0 +1,84 @@
+use log::error;
+use yew::prelude::*;
+
+use dydx_api::types::Ohlc;
+
+use crate::utils::api_utils::fetch_single_api_response;
+
+#[derive(Properties, PartialEq)]
+struct OhlcListProps {
+    ohlc_data: Vec<Ohlc>,
+}
+
+#[function_component(OhlcList)]
+fn ohlc_list(OhlcListProps { ohlc_data }: &OhlcListProps) -> Html {
+    let ohlc_data_html = ohlc_data.iter().map(|x| {
+        html! {
+            <tr>
+                <td>{x.open.clone()}</td>
+                <td>{x.high.clone()}</td>
+                <td>{x.low.clone()}</td>
+                <td>{x.close.clone()}</td>
+                <td>{x.timestamp.clone()}</td>
+            </tr>
+        }
+    });
+    html! {
+        <div>
+            <table class="table is-hoverable is-striped">
+                <thead>
+                    <tr>
+                        <th>{"Open"}</th>
+                        <th>{"High"}</th>
+                        <th>{"Low"}</th>
+                        <th>{"Close"}</th>
+                        <th>{"Timestamp"}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {for ohlc_data_html}
+                </tbody>
+            </table>
+        </div>
+    }
+}
+
+#[function_component(OhlcView)]
+pub fn ohlc_component() -> Html {
+    let market = "BTC-USD";
+    let resolution = "1MIN";
+    let ohlc_data = use_state(|| vec![]);
+    {
+        let ohlc_data = ohlc_data.clone();
+        use_effect_with_deps(
+            move |_| {
+                let ohlc_data = ohlc_data.clone();
+                wasm_bindgen_futures::spawn_local(async move {
+                    match fetch_single_api_response::<Vec<Ohlc>>(
+                        format!("/candles/{}/{}", market, resolution)
+                            .as_str(),
+                    )
+                        .await
+                    {
+                        Ok(fetched_data) => {
+                            ohlc_data.set(fetched_data);
+                        }
+                        Err(e) => {
+                            error!("{e}")
+                        }
+                    }
+                });
+            },
+            (),
+        );
+    }
+
+    html! {
+        <div class="section">
+            <div class="container">
+                <h1 class="title">{"Main page"}</h1>
+                <OhlcList ohlc_data={(*ohlc_data).clone()} />
+            </div>
+        </div>
+    }
+}
