10c1bab0b35d3e0ff0980896af3c77cf16dba6ee
sergey-astapov <serg.astapov@gmail.com>
2023-08-21 23:59:55 +0800

    - changes

--- dydx/dydx-api/src/types.rs
+++ dydx/dydx-api/src/types.rs
@@ -1,6 +1,10 @@
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 
+pub fn candles_url<'a>(market: &'a str, resolution: &'a str) -> String {
+    format!("/candles/{}/{}", market, resolution)
+}
+
 #[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
 pub struct OhlcResponse {
     pub list: Vec<Ohlc>,
