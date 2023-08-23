9ecfe94becc15598831532be62d5ee446cd920c1
sergey-astapov <serg.astapov@gmail.com>
2023-08-23 17:28:28 +0800

    - changes

--- dydx/dydx-bot/src/stats/average.rs
+++ dydx/dydx-bot/src/stats/average.rs
@@ -41,31 +41,87 @@ pub fn hma(v: Vec<Timeseries>, window_size: PeriodType) -> Vec<Timeseries> {
 }
 
 pub fn dema(v: Vec<Timeseries>, window_size: PeriodType) -> Vec<Timeseries> {
-    todo!()
+    let v0 = v[0].value.parse::<f64>().unwrap();
+    let mut avg = DEMA::new(window_size, &v0).unwrap();
+    convert(v, 1, |x| {
+        let value = avg.next(&x.value.parse::<f64>().unwrap());
+        Timeseries {
+            value: value.to_string(),
+            timestamp: x.timestamp,
+        }
+    })
 }
 
 pub fn dma(v: Vec<Timeseries>, window_size: PeriodType) -> Vec<Timeseries> {
-    todo!()
+    let v0 = v[0].value.parse::<f64>().unwrap();
+    let mut avg = DMA::new(window_size, &v0).unwrap();
+    convert(v, 1, |x| {
+        let value = avg.next(&x.value.parse::<f64>().unwrap());
+        Timeseries {
+            value: value.to_string(),
+            timestamp: x.timestamp,
+        }
+    })
 }
 
 pub fn rma(v: Vec<Timeseries>, window_size: PeriodType) -> Vec<Timeseries> {
-    todo!()
+    let v0 = v[0].value.parse::<f64>().unwrap();
+    let mut avg = RMA::new(window_size, &v0).unwrap();
+    convert(v, 1, |x| {
+        let value = avg.next(&x.value.parse::<f64>().unwrap());
+        Timeseries {
+            value: value.to_string(),
+            timestamp: x.timestamp,
+        }
+    })
 }
 
 pub fn swma(v: Vec<Timeseries>, window_size: PeriodType) -> Vec<Timeseries> {
-    todo!()
+    let v0 = v[0].value.parse::<f64>().unwrap();
+    let mut avg = SWMA::new(window_size, &v0).unwrap();
+    convert(v, 1, |x| {
+        let value = avg.next(&x.value.parse::<f64>().unwrap());
+        Timeseries {
+            value: value.to_string(),
+            timestamp: x.timestamp,
+        }
+    })
 }
 
 pub fn tema(v: Vec<Timeseries>, window_size: PeriodType) -> Vec<Timeseries> {
-    todo!()
+    let v0 = v[0].value.parse::<f64>().unwrap();
+    let mut avg = TEMA::new(window_size, &v0).unwrap();
+    convert(v, 1, |x| {
+        let value = avg.next(&x.value.parse::<f64>().unwrap());
+        Timeseries {
+            value: value.to_string(),
+            timestamp: x.timestamp,
+        }
+    })
 }
 
 pub fn tma(v: Vec<Timeseries>, window_size: PeriodType) -> Vec<Timeseries> {
-    todo!()
+    let v0 = v[0].value.parse::<f64>().unwrap();
+    let mut avg = TMA::new(window_size, &v0).unwrap();
+    convert(v, 1, |x| {
+        let value = avg.next(&x.value.parse::<f64>().unwrap());
+        Timeseries {
+            value: value.to_string(),
+            timestamp: x.timestamp,
+        }
+    })
 }
 
 pub fn trima(v: Vec<Timeseries>, window_size: PeriodType) -> Vec<Timeseries> {
-    todo!()
+    let v0 = v[0].value.parse::<f64>().unwrap();
+    let mut avg = TRIMA::new(window_size, &v0).unwrap();
+    convert(v, 1, |x| {
+        let value = avg.next(&x.value.parse::<f64>().unwrap());
+        Timeseries {
+            value: value.to_string(),
+            timestamp: x.timestamp,
+        }
+    })
 }
 
 pub fn vwma(v: Vec<Timeseries>, window_size: PeriodType) -> Vec<Timeseries> {
@@ -73,15 +129,39 @@ pub fn vwma(v: Vec<Timeseries>, window_size: PeriodType) -> Vec<Timeseries> {
 }
 
 pub fn vidya(v: Vec<Timeseries>, window_size: PeriodType) -> Vec<Timeseries> {
-    todo!()
+    let v0 = v[0].value.parse::<f64>().unwrap();
+    let mut avg = Vidya::new(window_size, &v0).unwrap();
+    convert(v, 1, |x| {
+        let value = avg.next(&x.value.parse::<f64>().unwrap());
+        Timeseries {
+            value: value.to_string(),
+            timestamp: x.timestamp,
+        }
+    })
 }
 
 pub fn wma(v: Vec<Timeseries>, window_size: PeriodType) -> Vec<Timeseries> {
-    todo!()
+    let v0 = v[0].value.parse::<f64>().unwrap();
+    let mut avg = WMA::new(window_size, &v0).unwrap();
+    convert(v, 1, |x| {
+        let value = avg.next(&x.value.parse::<f64>().unwrap());
+        Timeseries {
+            value: value.to_string(),
+            timestamp: x.timestamp,
+        }
+    })
 }
 
 pub fn wsma(v: Vec<Timeseries>, window_size: PeriodType) -> Vec<Timeseries> {
-    todo!()
+    let v0 = v[0].value.parse::<f64>().unwrap();
+    let mut avg = WSMA::new(window_size, &v0).unwrap();
+    convert(v, 1, |x| {
+        let value = avg.next(&x.value.parse::<f64>().unwrap());
+        Timeseries {
+            value: value.to_string(),
+            timestamp: x.timestamp,
+        }
+    })
 }
 
 fn convert<A, B>(v: Vec<A>, skip_n: usize, f: impl FnMut(A) -> B) -> Vec<B> {
