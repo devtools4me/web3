d60d7ad3c44ba501ad1c9acf3c97850857b4d8c8
sergey-astapov <serg.astapov@gmail.com>
2023-08-30 23:25:26 +0800

    - changes

--- dydx/dydx-ta/tests/run_together_check.rs
+++ dydx/dydx-ta/tests/run_together_check.rs
@@ -1,7 +1,18 @@
+use dydx_ta::indicators::run_together;
+
 #[cfg(test)]
 mod test {
+    use std::path::PathBuf;
+    use std::fs;
+
     #[test]
     fn test_run_together() {
+        let mut file_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        file_path.push("resources/test/BTC-USD-1DAY.json");
+
+        let contents = fs::read_to_string(file_path.as_path())
+            .expect("Should have been able to read the file");
+
         assert!(true);
     }
 }
